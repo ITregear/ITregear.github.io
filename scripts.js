b52b709e0efe3b340287fa7262b99a3600bfb937
ITregear <ivantregear@gmail.com>
2024-03-02 16:11:51 +0000

    added images and clickable tab layout

--- scripts.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('aboutMeBtn').addEventListener('click', function() {
-        window.location.href = 'about-me.html';
-    });
-});
